f8a8481611a7070c2ac7ed0153bf1ad992a57049
sumitgithub007 <66677424+sumitgithub007@users.noreply.github.com>
2022-01-22 17:59:00 +0530

    Update LoginClass.java

--- CircleSumit/src/test/java/stepDefinitions/LoginClass.java
+++ CircleSumit/src/test/java/stepDefinitions/LoginClass.java
@@ -13,15 +13,26 @@ public class LoginClass {
 		 @Given("User login to google")
 		public void user_login_to_google() {
 		    
+				 ChromeOptions chr = new ChromeOptions();
+			 chr.addArguments("--no-sandbox");
+			 chr.addArguments("--headless");
+			 chr.addArguments("--no-sandbox");
+			 chr.addArguments("--disable-dev-shm-usage");
+			 
+			 chr.addArguments("--disable-gpu");
+			 
+				  //  
+			 
 			 System.setProperty("webdriver.chrome.driver","/usr/local/bin/chromedriver");
 
-			 WebDriver driver = new ChromeDriver();
+			 WebDriver driver = new ChromeDriver(chr);
 
 			 driver.get("https://mvnrepository.com");
 
 			 driver.close();	 
 			 
 		 System.out.println("Hello namo");
+		 
 		 
 		 
 		 }
